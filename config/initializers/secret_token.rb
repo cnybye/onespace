5aacb25a2febe48ed4f8d75112b76b04bc1aba8a
cnybye <cnybye@hotmail.com>
2011-02-07 13:12:23 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onespace::Application.config.secret_token = '5604ac0b98ee5d25a044376aa5265b15b72a556ac8db3e9ac01bbe69cc20dfe352003bff97a4b2f004c925cd6c94b0f3e0b6a850d2bf8d7f2daf4b4554f6b715'
